f048335d0ab1bb9ecb1857e6c1af1dbd73d29407
Ülvi Laçınov <ulvi.lacin@btarge.com>
2020-06-16 20:32:07 +0300

    featuer/adding axios and post order to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
-
+import axios from "../../axios-order";
 
 const INGREDIENT_PRICES = {
     salad: 0.3,
@@ -77,7 +77,27 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert("You continue!!!");
+      //  alert("You continue!!!");
+      const order = {
+          ingredients: this.state.ingredients,
+          price: this.state.totalPrice,
+          customer: {
+              name: "Ulvi Laçınov",
+              address: {
+                  street: "Yumurtacı Abdı bey",
+                  zipCode: "34011",
+                  country: "Turkey"
+              },
+              email: "ulvilacinov@gmail.com"
+          },
+          deliveryMethod: "fastest"
+      }
+      axios.post("/orders.json",order)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => console.log(error));
+
     }
 
     render() {
