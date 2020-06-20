cc9450115fe534c89c72603c8b58b97ce2158813
Ülvi Laçınov <ulvi.lacin@btarge.com>
2020-06-21 00:33:49 +0300

    feat/adding Input component, changed ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "../../../axios-order";
 import Button from "../../../components/UI/Button/Button";
+import Input from "../../../components/UI/Input/Input";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.module.css";
 
@@ -45,10 +46,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type='text' name='name' placeholder='Your Name' />
-                <input className={classes.Input} type='text' name='email' placeholder='Your Name' />
-                <input className={classes.Input} type='text' name='street' placeholder='Your Street' />
-                <input className={classes.Input} type='text' name='postal ' placeholder='Your Postal Code' />
+                <Input inputtype="input" type='text' name='name' placeholder='Your Name' />
+                <Input inputtype="input" type='text' name='email' placeholder='Your Name' />
+                <Input inputtype="input" type='text' name='street' placeholder='Your Street' />
+                <Input inputtype="input" type='text' name='postal ' placeholder='Your Postal Code' />
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         )
