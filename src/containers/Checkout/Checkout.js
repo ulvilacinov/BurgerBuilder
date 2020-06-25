0875f53b9096b3fe2f900d7dfc0c8e8fb47e432c
Ülvi Laçınov <ulvi.lacin@btarge.com>
2020-06-25 21:26:48 +0300

    refactor/creating reducer and actionTypes, changing ingredients and price state to redux

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,26 +3,9 @@ import CheckoutSummary from "../../components/Burger/CheckoutSummary/CheckoutSum
 import { Route } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 
+import {connect} from "react-redux";
+
 class Checkout extends Component {
-    state = {
-        ingredients: {},
-        totalPrice: 0
-    }
-
-    componentWillMount() {
-        const ingredients = {};
-        let price = 0;
-        const query = new URLSearchParams(this.props.location.search);
-        for (let param of query.entries()) {
-            if (param[0] === 'price') {
-                price = param[1];
-            } else {
-                ingredients[param[0]] = +param[1];
-            }
-        }
-        this.setState({ ingredients: ingredients, totalPrice: price });
-    }
-
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
@@ -35,19 +18,22 @@ class Checkout extends Component {
         return (
             <div>
                 <CheckoutSummary
-                    ingredients={this.state.ingredients}
+                    ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                 />
                 <Route
                     path={this.props.match.url + "/contact-data"}
-                    render={(props) => (<ContactData
-                        ingredients={this.state.ingredients}
-                        totalPrice={this.state.totalPrice}
-                        {...props} />)} />
+                   component={ContactData} />
             </div>
         );
     }
 }
 
-export default Checkout;
+const mapStateToProps = state => {
+    return {
+        ings : state.ingredients,
+    }
+}
+
+export default connect(mapStateToProps)(Checkout);
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,8 @@ import Button from "../../../components/UI/Button/Button";
 import Input from "../../../components/UI/Input/Input";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.module.css";
+import {connect} from "react-redux";
+
 
 class ContactData extends Component {
     state = {
@@ -196,4 +198,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
